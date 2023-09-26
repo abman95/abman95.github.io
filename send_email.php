7b06c79f3d2a273b9ad02cc2c5a28b9f98e17675
abman95 <abdullah.soeguet@outlook.de>
2023-09-26 22:20:31 +0200

    Update Portfolio from errors

--- send_email.php
+++ send_email.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST["name"];
@@ -16,22 +15,3 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     exit;
 }
 ?>
-=======
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
-    
-    $to = "abdullah.soeguet@outlook.de"; // Ihre E-Mail-Adresse hier
-    $subject = "Kontaktanfrage von $name";
-    $headers = "From: $email";
-    
-    mail($to, $subject, $message, $headers);
-    
-    // Optional: Weiterleitung oder Bestätigungsseite anzeigen
-    header("Location: confirmation.html"); // Ersetzen Sie 'confirmation.html' durch den Dateinamen Ihrer Bestätigungsseite
-    exit;
-}
-?>
->>>>>>> 07af7c48b085646b2f0ed085b26cf22094ad3feb
